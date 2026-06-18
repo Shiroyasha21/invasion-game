1e0d3406d52502ee2f5b81240abb1a2464449793
Zeke <red.ezequiel@gmail.com>
2026-06-19 05:35:10 +0800

    Fix WavePreview: move drawing to Node2D child, fix CanvasLayer draw errors

--- invasion-game/scripts/ui/wave_preview.gd
+++ invasion-game/scripts/ui/wave_preview.gd
@@ -6,6 +6,13 @@ const PULSE_SPEED := 2.0
 var _directions: Array[int] = []
 var _pulse_timer: float = 0.0
 var _hex_grid: HexGridNode
+var _draw_node: Node2D
+
+
+func _ready() -> void:
+	_draw_node = Node2D.new()
+	add_child(_draw_node)
+	_draw_node.draw.connect(_on_draw)
 
 
 func init(grid: HexGridNode) -> void:
@@ -20,17 +27,17 @@ func show_preview(directions: Array[int]) -> void:
 func hide_preview() -> void:
 	_directions = []
 	visible = false
-	queue_redraw()
+	_draw_node.queue_redraw()
 
 
-func _process(delta: float) -> void:
+func _process(delta: float) -> void:  # delta used below
 	if _directions.is_empty():
 		return
 	_pulse_timer += delta
-	queue_redraw()
+	_draw_node.queue_redraw()
 
 
-func _draw() -> void:
+func _on_draw() -> void:
 	if _directions.is_empty() or _hex_grid == null:
 		return
 
@@ -39,7 +46,7 @@ func _draw() -> void:
 	var center := _hex_grid.hex_grid_to_pixel(Vector2i.ZERO)
 
 	for dir in _directions:
-		var dir_vec := HexGrid.DIRECTIONS[dir]
+		var dir_vec: Vector2i = HexGrid.DIRECTIONS[dir]
 		var edge_hex := Vector2i(dir_vec.x * (_hex_grid.grid_radius + 2), dir_vec.y * (_hex_grid.grid_radius + 2))
 		var edge_px := _hex_grid.hex_grid_to_pixel(edge_hex)
 
@@ -47,17 +54,14 @@ func _draw() -> void:
 		var arrow_start := edge_px
 		var arrow_end := edge_px + toward_center * 80.0
 
-		# Shaft
-		draw_line(arrow_start, arrow_end, color, 4.0)
+		_draw_node.draw_line(arrow_start, arrow_end, color, 4.0)
 
-		# Arrowhead
 		var perp := Vector2(-toward_center.y, toward_center.x)
-		draw_colored_polygon(PackedVector2Array([
+		_draw_node.draw_colored_polygon(PackedVector2Array([
 			arrow_end + toward_center * 20.0,
 			arrow_end + perp * 14.0,
 			arrow_end - perp * 14.0,
 		]), color)
 
-		# Direction label
-		draw_string(ThemeDB.fallback_font, arrow_start - toward_center * 20.0,
+		_draw_node.draw_string(ThemeDB.fallback_font, arrow_start - toward_center * 20.0,
 			HexGrid.DIR_NAMES[dir], HORIZONTAL_ALIGNMENT_CENTER, -1, 22, color)
